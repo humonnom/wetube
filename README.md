bc1f060c4e96784d09e3c26e54dc8451001ee9c2
homonnom <josojoso0702@gmail.com>
2019-12-02 16:48:35 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # wetube
- Cloning Youtube with Vanilla and Node JS
+
+Cloning Youtube with Vanilla and Node JS
+
+## Pages
+
+- [] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [] User Detail
+- [] Edit Profile
+- [x] Change Password
+- [x] Upload
+- [] Video Detail
+- [x] Edit Video
---
+++ db.js
@@ -0,0 +1,41 @@
+export const videos = [
+  {
+    id: 23908,
+    title: "Video awesome",
+    description: "This is something I like",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 19478123,
+      name: "Jueun",
+      email: "joso0702@naver.com"
+    }
+  },
+  {
+    id: 234234,
+    title: "Video(2)",
+    description: "This is something I like",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 34500,
+      name: "Jueun",
+      email: "joso0702@naver.com"
+    }
+  },
+  {
+    id: 3451345,
+    title: "Just Just Video",
+    description: "This is something I like",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1345444,
+      name: "Jueun",
+      email: "joso0702@naver.com"
+    }
+  }
+];
